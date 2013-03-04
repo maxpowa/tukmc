a44e07444f334c22d766063a71c663b033095586
maxpowa <maxpowa1@gmail.com>
2013-03-03 19:59:44 -0700

    You'll never guess it. COLOR CONFIG!

--- maxpowa/tukmc/mod_TukMC.java
+++ maxpowa/tukmc/mod_TukMC.java
@@ -70,28 +70,44 @@ public class mod_TukMC {
 	
 	public static void saveColorSettings() {
 		NBTTagCompound cmp = IOUtils.getTagCompoundInFile(cacheFile);
-		cmp.setInteger("redRaw", TukMCReference.RED);
-		cmp.setInteger("greenRaw", TukMCReference.GREEN);
-		cmp.setInteger("blueRaw", TukMCReference.BLUE);
-	    Color c = new Color(TukMCReference.RED, TukMCReference.GREEN, TukMCReference.BLUE);
-	    TukMCReference.BOX_INNER_COLOR = c.getRGB();
-	    cmp.setInteger("hexFill", TukMCReference.BOX_INNER_COLOR);
+		cmp.setInteger("red_inner", TukMCReference.RED_INNER);
+		cmp.setInteger("green_inner", TukMCReference.GREEN_INNER);
+		cmp.setInteger("blue_inner", TukMCReference.BLUE_INNER);
+		cmp.setInteger("red_outer", TukMCReference.RED_OUTER);
+		cmp.setInteger("green_outer", TukMCReference.GREEN_OUTER);
+		cmp.setInteger("blue_outer", TukMCReference.BLUE_OUTER);
+	    Color c_inner = new Color(TukMCReference.RED_INNER, TukMCReference.GREEN_INNER, TukMCReference.BLUE_INNER);
+	    TukMCReference.BOX_INNER_COLOR = c_inner.getRGB();
+	    cmp.setInteger("hex_inner", TukMCReference.BOX_INNER_COLOR);
+	    Color c_outer = new Color(TukMCReference.RED_OUTER, TukMCReference.GREEN_OUTER, TukMCReference.BLUE_OUTER);
+	    TukMCReference.BOX_OUTLINE_COLOR = c_outer.getRGB();
+	    cmp.setInteger("hex_outline", TukMCReference.BOX_OUTLINE_COLOR);
 		IOUtils.injectNBTToFile(cmp, cacheFile);
+	    TukMCReference.BOX_HIGHLIGHT_COLOR = Color.getColor(null, TukMCReference.BOX_INNER_COLOR).brighter().brighter().brighter().getRGB();
 	}
 
 	public static void loadColorSettings() {
 		NBTTagCompound cmp = IOUtils.getTagCompoundInFile(cacheFile);
-		TukMCReference.RED = cmp.hasKey("redRaw") ? cmp.getInteger("redRaw") : 0;
-		TukMCReference.GREEN = cmp.hasKey("greenRaw") ? cmp.getInteger("greenRaw") : 0;
-		TukMCReference.BLUE = cmp.hasKey("blueRaw") ? cmp.getInteger("blueRaw") : 0;
-	    TukMCReference.BOX_INNER_COLOR = cmp.hasKey("hexFill") ? cmp.getInteger("hexFill") : 0x1A1A1A;
+		TukMCReference.RED_INNER = cmp.hasKey("red_inner") ? cmp.getInteger("red_inner") : 26;
+		TukMCReference.GREEN_INNER = cmp.hasKey("green_inner") ? cmp.getInteger("green_inner") : 26;
+		TukMCReference.BLUE_INNER = cmp.hasKey("blue_inner") ? cmp.getInteger("blue_inner") : 26;
+		TukMCReference.RED_OUTER = cmp.hasKey("red_outer") ? cmp.getInteger("red_outer") : 150;
+		TukMCReference.GREEN_OUTER = cmp.hasKey("green_outer") ? cmp.getInteger("green_outer") : 154;
+		TukMCReference.BLUE_OUTER = cmp.hasKey("blue_outer") ? cmp.getInteger("blue_outer") : 165;
+	    TukMCReference.BOX_INNER_COLOR = cmp.hasKey("hex_inner") ? cmp.getInteger("hex_inner") : 0x1A1A1A;
+	    TukMCReference.BOX_OUTLINE_COLOR = cmp.hasKey("hex_outline") ? cmp.getInteger("hex_outline") : 0x969AA5;
 	}
 	
 	public static void defaultColorSettings() {
 	    TukMCReference.BOX_INNER_COLOR = 0x1A1A1A;
-	    TukMCReference.RED = 26;
-	    TukMCReference.GREEN = 26;
-	    TukMCReference.BLUE = 26;
+	    TukMCReference.BOX_HIGHLIGHT_COLOR = 0x4C4C4C;
+	    TukMCReference.BOX_OUTLINE_COLOR = 0x969AA5;
+	    TukMCReference.RED_INNER = 26;
+	    TukMCReference.GREEN_INNER = 26;
+	    TukMCReference.BLUE_INNER = 26;
+	    TukMCReference.RED_OUTER =  150;
+	    TukMCReference.GREEN_OUTER = 154;
+	    TukMCReference.BLUE_OUTER = 165;
 	}
 	
 	public static void registerOpenWebsite(String s) {
