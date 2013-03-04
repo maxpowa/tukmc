506f0a8c0908dcfba3987aa7bde20522e451716e
maxpowa <maxpowa1@gmail.com>
2013-03-03 19:59:23 -0700

    Color config again!

--- maxpowa/tukmc/TukMCReference.java
+++ maxpowa/tukmc/TukMCReference.java
@@ -7,10 +7,14 @@ public class TukMCReference {
 	public static int BOX_HIGHLIGHT_COLOR = 0x4C4C4C;
 	public static int BOX_EFFECT_OUTLINE_COLOR = 0x8d60a6;
 	
-	public static int RED = 0;
-	public static int GREEN = 0;
-	public static int BLUE = 0;
-
+	public static int RED_INNER = 0;
+	public static int GREEN_INNER = 0;
+	public static int BLUE_INNER = 0;
+	
+	public static int RED_OUTER = 0;
+	public static int GREEN_OUTER = 0;
+	public static int BLUE_OUTER = 0;	
+	
 	public static final String MC_VERSION = "1.4.7";
 
 	public static final String VERSION = "1.0.2";
