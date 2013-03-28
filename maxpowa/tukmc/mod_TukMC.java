1a7f6a1e7c2ba3adde43e7705e202ae8aff71a52
maxpowa <maxpowa1@gmail.com>
2013-03-28 12:13:02 -0600

    Version Revision
    Yes, I know I missed one version :P It is in the compiled version
    though.

--- maxpowa/tukmc/mod_TukMC.java
+++ maxpowa/tukmc/mod_TukMC.java
@@ -20,11 +20,11 @@ import cpw.mods.fml.common.registry.TickRegistry;
 
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = "tukmc_Vz", name = "TukMC", version = "Version [2.5] for 1.5.1")
+@Mod(modid = "tukmc_Vz", name = "TukMC", version = "Version [2.7] for 1.5.1")
 public class mod_TukMC {
 
 	public static String MC_VERSION = "1.5.1";
-	public static String TK_VERSION = "2.5";
+	public static String TK_VERSION = "2.7";
 	
 	public static File cacheFile;
 
